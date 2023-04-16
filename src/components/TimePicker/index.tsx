2c0b5de0fbbfb4f2cac06d105e2eab7135cda922
mina <yegrinaaa@gmail.com>
2023-04-17 01:25:31 +0900

    [Feat] 메인 UI 틀 완료

--- src/components/TimePicker/index.tsx
+++ src/components/TimePicker/index.tsx
@@ -1,33 +1,63 @@
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import { TimeClock } from "@mui/x-date-pickers"
-import React, { FC, useState } from "react"
+import React, { useState } from "react"
 import dayjs, { Dayjs } from "dayjs"
 import styled from "@emotion/styled"
+import { TimeView } from "@mui/x-date-pickers/models/views"
 
-interface TimePickerProps {
-  handleTime: (hour: string, min: string) => void
-}
+// interface TimePickerProps {
+//   handleTime: (hour: string, min: string) => void
+// }
 
-export const TimePicker: FC<TimePickerProps> = (props) => {
+export const Time = () => {
   const [value, setValue] = useState<Dayjs | null>(dayjs())
+  const [timeOption, setTimeOption] = useState<TimeView>("hours")
+
+  const handleTimeOption = (option: TimeView) => {
+    setTimeOption(option)
+  }
+
+  console.log(value?.hour())
+  console.log(value?.hour() && value?.hour() + 12)
 
   return (
     <Wrapper>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <TimeClock views={["hours", "minutes"]} value={value} onChange={(newValue) => setValue(newValue)} />
+        <TimeClock views={[timeOption]} value={value} onChange={(newValue) => setValue(newValue)} />
       </LocalizationProvider>
+      <div style={{ display: "inline-block" }}>
+        <ButtonWrap>
+          <Button onClick={() => handleTimeOption("hours")}>am/pm</Button>
+          <Button onClick={() => handleTimeOption("hours")}>hours</Button>
+          <Button onClick={() => handleTimeOption("minutes")}>minutes</Button>
+        </ButtonWrap>
+      </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  .MuiTimeClock-root {
-    display: inline-block;
-    width: 220px;
+  text-align: center;
 
-    & .MuiClock-root {
-      margin: 0;
-    }
+  .MuiClock-clock {
+    background-color: #d2eaff;
   }
+`
+
+const ButtonWrap = styled.div`
+  display: flex;
+  grid-gap: 10px;
+  margin-top: 30px;
+  &::last-item {
+    margin-right: 0;
+  }
+`
+
+const Button = styled.span`
+  padding: 10px;
+  font-size: 1.5rem;
+  border-radius: 8px;
+  background-color: pink;
+  cursor: pointer;
 `
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react"
 import styled from "@emotion/styled"
-import { TimePicker } from "../components/TimePicker"
+import { Time } from "../components/TimePicker"
 import { TextField } from "@mui/material"
 
 const Main = () => {
   const [title, setTitle] = useState<string>("")
-  const [time, setTime] = useState({ hour: "", min: "" })
 
   const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
     setTitle(value)
   }
 
-  const handleTime = (hour: string, min: string) => {
-    setTime({ ...time, hour, min })
-  }
   return (
     <MainContainer>
       <Title>today's schedule</Title>
       <Wrap>
-        <TextField id="standard-basic" label="스케줄 이름" variant="standard" value={title} onChange={handleTitle} />
+        <TextField
+          className="title-input"
+          id="standard-basic"
+          label="스케줄 이름"
+          variant="standard"
+          value={title}
+          onChange={handleTitle}
+        />
         <TimePickerWrap>
-          <TimePicker handleTime={handleTime} />
+          <Time />
         </TimePickerWrap>
       </Wrap>
     </MainContainer>
@@ -33,19 +36,22 @@ export default Main
 const MainContainer = styled.div`
   padding: 20px 30px;
   height: 100vh;
+  text-align: center;
+  background-color: #f0f0e8;
+
+  .title-input {
+    width: 300px;
+  }
 `
 const Wrap = styled.div`
-  padding: 20px 0;
-  width: 100%;
-  max-width: 810px;
+  padding-top: 80px;
 `
 
 const Title = styled.p`
-  font-size: 6.25rem;
+  font-size: 4rem;
   font-weight: 800;
 `
 
 const TimePickerWrap = styled.div`
   padding-top: 30px;
-  width: 320px;
 `
